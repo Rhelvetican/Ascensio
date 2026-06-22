874564ad626831f60863363d39a96dc4bf3ee74f
Rhelvetican <bionicvnb@gmail.com>
2026-06-22 07:09:41 +0700

    small improvements

--- assets/1x/resize.py
+++ assets/1x/resize.py
@@ -1,24 +1,25 @@
-import sys
-from PIL import Image
 import os
-import time
+import sys
+
+from PIL import Image
 
 
-
-
-def upscale_pixel_art(input_image, output_directory, input_image_path):
+def upscale_pixel_art(input_image: Image, output_directory: str, input_image_path: str):
     # Double the size
-    
+
     new_size = (input_image.width * 2, input_image.height * 2)
-    resized_image = input_image.resize(new_size, Image.NEAREST)  # NEAREST resampling preserves pixelation
+    resized_image = input_image.resize(
+        new_size, Image.NEAREST
+    )  # NEAREST resampling preserves pixelation
 
     # Save the resized image
     filename = os.path.basename(input_image_path)
     output_image_path = os.path.join(output_directory, filename)
     resized_image.save(output_image_path)
 
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
         print("Usage: py resize.py <input_image>")
         print(os.getcwd())
         sys.exit(1)
